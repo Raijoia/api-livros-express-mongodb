1a041280bb419e57766d7495934f3c382efe18bc
Raí Joia <megac154@gmail.com>
2024-01-02 14:41:36 -0300

    adicionado rotas e dependencia nodemon

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from 'http';
 
 const PORT = 8080;
 
+const rotas = {
+  '/': "Curso de Node.js",
+  '/livros': "Listagem de livros",
+  '/autores': "Listagem de autores"
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
+  res.end(rotas[req.url] || "Rota não encontrada: " + req.url);
 })
 
 server.listen(PORT, () => {
